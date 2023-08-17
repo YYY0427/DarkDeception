7c2978d0542c0689b82bb563201b478900f84702
わしじゃよわし <114457086+wasizyayowasi@users.noreply.github.com>
2023-08-17 13:52:37 +0900

    ミスってmainで変更した

--- DarkDeception/Assets/Scenes/NumberOfCrystalsRemaining.cs
+++ DarkDeception/Assets/Scenes/NumberOfCrystalsRemaining.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class NumberOfCrystalsRemaining : MonoBehaviour
 {
     public GameObject _objText;
     public TextMeshProUGUI _text;
     public GameObject _crystal;
+    public GameObject _fadePrefab;
 
     int count = 0;
+    readonly float _fadeSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,5 +30,18 @@ public class NumberOfCrystalsRemaining : MonoBehaviour
             count++;
         }
         _text.text = "remaining  " + count.ToString();
+
+        if(count <= 0)
+        {
+            StartCoroutine(nameof(LoadScene));
+        }
     }
+
+    IEnumerator LoadScene()
+    {
+        Instantiate(_fadePrefab);
+        yield return new WaitForSeconds(_fadeSpeed);
+        SceneManager.LoadScene("clearScene");
+    }
+
 }
---
+++ DarkDeception/Assets/Scenes/clear/clear.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class clear : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject _objText;
+    public TextMeshProUGUI _text;
+    public GameObject _fadePrefab;
+
+    readonly float _fadeSpeed = 1f;
+
+    void Start()
+    {
+        _objText = GameObject.Find("TitleText");
+        _text = _objText.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _text.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    public void OnClickStartButton()
+    {
+        StartCoroutine(nameof(LoadScene));
+    }
+
+    IEnumerator LoadScene()
+    {
+        Instantiate(_fadePrefab);
+        yield return new WaitForSeconds(_fadeSpeed);
+        SceneManager.LoadScene("titleScene");
+    }
+}
