f5f3dbb1fa81b2db039e2836fbf63433240d7f72
niyugitHub <ryutansmr@icloud.com>
2023-08-28 18:53:15 +0900

    やられたときの演出のシーン追加

--- DarkDeception/Assets/Scenes/KnockDownScene/EnemyStaging.cs
+++ DarkDeception/Assets/Scenes/KnockDownScene/EnemyStaging.cs
@@ -14,18 +14,12 @@ public class EnemyStaging : MonoBehaviour
     {
         obj = this.GameObject();
         cameraObj = GameObject.Find("Main Camera");
-
-        obj.transform.position = cameraObj.transform.position;
-        obj.transform.position += new Vector3(0, -2.0f, 1.0f);
-
-        obj.transform.rotation = cameraObj.transform.rotation;
-        obj.transform.Rotate(0, 180, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (obj.transform.position.y < cameraObj.transform.position.y - 0.8f)
+        if (obj.transform.position.y < 0)
         {
             obj.transform.Translate(0, 0.007f, 0);
         }
