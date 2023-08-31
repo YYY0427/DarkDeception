7d1263449be739bb5058d376a6c89333cf41871e
わしじゃよわし <114457086+wasizyayowasi@users.noreply.github.com>
2023-08-31 16:39:54 +0900

    仮更新

--- DarkDeception/Assets/Scenes/gameover/reopen.cs
+++ DarkDeception/Assets/Scenes/gameover/reopen.cs
@@ -33,8 +33,9 @@ public class reopen : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        _text.color = new Color(0.68f, 0.68f, 0.69f, 1.0f);
-        _text.fontSize = 24;
+        //_text.color = new Color(0.68f, 0.68f, 0.69f, 1.0f);
+        _text.color = new Color(0.86f, 0.86f, 0.86f, 1.0f);
+        _text.fontSize = 30;
     }
 
     IEnumerator LoadScene()
