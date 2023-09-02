ecb8c7d4b714842edeebc81f708eca8f37cadcaa
わしじゃよわし <114457086+wasizyayowasi@users.noreply.github.com>
2023-09-02 23:25:08 +0900

    更新

---
+++ DarkDeception/Assets/Scenes/gameover/BGM.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (reopen.clickButton)
+        {
+            audio.volume = System.Math.Max(audio.volume - 0.01f, 0.0f);
+            if (audio.volume <= 0.0f)
+            {
+                reopen.clickButton = false;
+            }
+        }
+        else
+        {
+            audio.volume = System.Math.Min(audio.volume + 0.001f, 0.3f);
+            Debug.Log(audio.volume);
+        }
+    }
+
+
+}
