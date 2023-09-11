87aac6dd0bd2117dbe0f4b2b47fda4a5029b1bf5
Gaxx <lwt5gy707g@gmail.com>
2023-09-11 12:03:49 +0900

    捕まった時サウンドの追加

--- DarkDeception/Assets/Scenes/KnockDownScene/KnockDownScript.cs
+++ DarkDeception/Assets/Scenes/KnockDownScene/KnockDownScript.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class KnockDownScript : MonoBehaviour
 {
     private int _nextSceneFrame;    // 次のシーンに移行するまでの時間
+    public AudioSource _audioSource;
     void Start()
     {
         _nextSceneFrame = 150;
+        // AudioSourceのコンポーネント取得
+        _audioSource = GetComponent<AudioSource>();
+        // 音楽再生
+        _audioSource.Play();
     }
 
     // Update is called once per frame
