e84387b26809bb6904786403b8cc2b50f71b95af
YYY0427 <yomih15427@gmail.com>
2023-09-11 09:41:41 +0900

    ロード画面の修正

---
+++ DarkDeception/Assets/Scenes/sceneSelect/percent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class percent : MonoBehaviour
+{
+    private TextMeshProUGUI cardNameText;
+
+    [SerializeField]
+    private Slider slider;
+    private void Start()
+    {
+        cardNameText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cardNameText.SetText("{0}%", slider.value * 100.0f);
+    }
+}
